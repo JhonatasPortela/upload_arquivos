4a2317b042de10a03080e9c7f49316fe515072cc
Jhonatas Portela <jhonatasportela1997@gmail.com>
2025-05-10 20:22:50 -0300

    Adicionar e remover tags

--- scripts.js
+++ scripts.js
@@ -5,15 +5,17 @@ uploadBtn.addEventListener("click", () =>  {
     inputUpload.click();
 });
 
-function lerConteudoDoArquivo(arquivo) {
+function lerConteudoDoArquivo(arquivo){
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
-        leitor.onload = ()=> {
-            resolve({url: leitor.result, nome: arquivo.name})
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
         }
+
         leitor.onerror = () => {
-            reject(`Erro na leitura do arquivo: ${arquivo.name}`);
+            reject(`Erro ao ler o arquivo ${arquivo.name}`);
         }
+
         leitor.readAsDataURL(arquivo);
     });
 }
@@ -23,14 +25,47 @@ const nomeDaImagem = document.querySelector(".container-imagem-nome p");
 
 inputUpload.addEventListener("change", async (evento) => {
     const arquivo = evento.target.files[0];
-    if (arquivo) {
-        try {
+    if(arquivo){
+        try{
             const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
             imagemPrincipal.src = conteudoDoArquivo.url;
             nomeDaImagem.textContent = conteudoDoArquivo.nome;
-        } catch (erro) {
+        } catch(erro){
             console.error("Erro na leitura do arquivo");
+        } 
+    }
+})
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener("keypress", (evento) => {
+    if(evento.key === "Enter"){
+        evento.preventDefault();
+        const valorTag = inputTags.value.trim();
+        if(valorTag !== ""){
+            const novaTag = document.createElement("li");
+            novaTag.innerHTML = `<p>${valorTag}</p><img src="img/close-black.svg" class="remove-tag">`;
+            listaTags.appendChild(novaTag);
+            inputTags.value = "";
         }
     }
 });
 
+listaTags.addEventListener("click", (evento) => {
+    if(evento.target.classList.contains("remove-tag")){
+        const tagParaRemover = evento.target.parentElement;
+        listaTags.removeChild(tagParaRemover);
+    }
+});
+
+const tagsDisponiveis = ["Front-end", "Back-end", "Fullstack", "JavaScript", "HTML", "CSS", "React", "Node.js", "Python", "Java"];
+
+async function verificaTag(tagTexto){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 1000);
+
+    })
+}
